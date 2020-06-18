87c40b8d883938876509e117ed7c42aded51b85e
sarazhan <67008676+sarazhan@users.noreply.github.com>
2020-06-18 18:19:44 +0200

    Update gioco.py

--- gioco.py
+++ gioco.py
@@ -5,9 +5,13 @@ pygame.init()
 SCREEN_W = 900
 SCREEN_H = 700
 
-BEN_IMAGE = pygame.image.load("img/ben1.png")
+BEN1_IMAGE = pygame.image.load("img/ben1.png")
 BEN2_IMAGE = pygame.image.load("img/ben2.png")
-BEN_STARTING_POSITION = (20, 550)
-BEN_THRUST = 3
-BEN_SPEED = 4
+BEN1_STARTING_POSITION = (20, 550)
+BEN1_THRUST = 3
+BEN1_SPEED = 4
 GRAVITY = 0.1
+
+LEANDRO_IMAGE = pygame.image.load("img/Leandro.png")
+LEANDRO_SIZE = LEANDRO_IMAGE.get_size()
+LEANDRO_POSITION = (SCREEN_W / 2 - LEANDRO_IMAGE[0] / 2, SCREEN_H / 2 - LEANDRO_IMAGE[1] / 2 -20)
