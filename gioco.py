4bac1ea051c4e55d73ce4794d1851dffc59f34ac
RodolfiSimone <66484586+RodolfiSimone@users.noreply.github.com>
2020-06-18 18:12:09 +0200

    Update gioco.py

--- gioco.py
+++ gioco.py
@@ -4,3 +4,10 @@ pygame.init()
 
 SCREEN_W = 900
 SCREEN_H = 700
+
+BEN_IMAGE = pygame.image.load("img/ben1.png")
+BEN2_IMAGE = pygame.image.load("img/ben2.png")
+BEN_STARTING_POSITION = (20, 550)
+BEN_THRUST = 3
+BEN_SPEED = 4
+GRAVITY = 0.1
