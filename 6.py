e3a7f4e21557f43519fd9a31bc4ef466cf847b84
sarazhan <67008676+sarazhan@users.noreply.github.com>
2020-07-23 17:26:53 +0200

    Update 6.py

--- 6.py
+++ 6.py
@@ -3,8 +3,7 @@ import random
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-AZZURRO = (0, 128, 255)
-ROSSO = (255, 0, 0)
+BLUE = (0, 128, 255)
 screen_width = 900
 screen_height = 700
 screen = pygame.display.set_mode([screen_width, screen_height])
@@ -15,8 +14,7 @@ v = 0
 
 all_sprites_list = pygame.sprite.Group()
 morte_list = pygame.sprite.Group()
-
-todraw = pygame.sprite.Group()
+draw = pygame.sprite.Group()
 
 class Block(pygame.sprite.Sprite):
     def __init__(self, color):
@@ -26,9 +24,7 @@ class Block(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
     def update(self):
         self.rect.x -= velocita
-
-
-        
+      
 class Player(pygame.sprite.Sprite):
     move_x = 0
     move_y = 0
@@ -36,11 +32,11 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((30, 20))
-        self.image.fill(AZZURRO)
+        self.image.fill(BLUE)
         self.rect = self.image.get_rect()
         self.rect.x = 600
         self.rect.y = 680
-        todraw.add(self)
+        draw.add(self)
     def update(self):
         self.rect.x += self.move_x
         self.rect.y += self.move_y
@@ -52,19 +48,14 @@ pygame.init()
 
 player = Player()
 all_sprites_list.add(player)
-
 font = pygame.font.SysFont("brittanic", 25)
 scritta = font.render("Schiva più blocchi che puoi !!!", True, BLACK)
-
-
-    
 
 clock = pygame.time.Clock()
 
 player.rect.x = 150
 
 while True:
-
     s +=1
     if s == 100000:
         salto -=1
@@ -86,7 +77,6 @@ while True:
             if event.key == pygame.K_DOWN: 
                 onground = False
                 player.move_y = +10
-            
         if event.type == pygame.KEYUP:  
             if event.key == pygame.K_UP:
                 player.move_y = 0
@@ -103,7 +93,6 @@ while True:
         morte_list.add(morte)
         all_sprites_list.add(morte)
 
-           
     for blocco in morte_list:
         morte_hit_list = pygame.sprite.spritecollide(player, morte_list, True)
         for giocatore in morte_hit_list:
@@ -113,12 +102,11 @@ while True:
             s = 0
             v = 0
         if blocco.rect.x < 50:
-            all_sprites_list.remove(blocco)			
-
-        
+            all_sprites_list.remove(blocco)		
+            
     screen.fill(WHITE)
     all_sprites_list.draw(screen)
     screen.blit(scritta,(100, 20))
     pygame.display.flip()
     pygame.display.set_caption("Le avventure di Ben: livello 6")
-    clock.tick(60)     
+    clock.tick(60)
