6dbf79f4f5d2fae43cc7f0adef5c74e16f288b56
RodolfiSimone <66484586+RodolfiSimone@users.noreply.github.com>
2020-07-24 21:20:26 +0200

    Update 6.py

--- 6.py
+++ 6.py
@@ -8,7 +8,7 @@ screen_width = 900
 screen_height = 700
 screen = pygame.display.set_mode([screen_width, screen_height])
 Eroe = pygame.image.load("img/eroe.png")
-EROE = pygame.transform.scale(Eroe,(65, 60))
+EROE = pygame.transform.scale(Eroe,(60, 55))
 velocita = 2.5
 v = 0
 s = 0
@@ -17,6 +17,19 @@ score = 0
 all_sprites_list = pygame.sprite.Group()
 morte_list = pygame.sprite.Group()
 draw = pygame.sprite.Group()
+plats = pygame.sprite.Group()
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((25, 25))
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        plats.add(self)
+    def update(self):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
 
 class Block(pygame.sprite.Sprite):
     def __init__(self, color):
@@ -35,13 +48,84 @@ class Player(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = EROE
         self.rect = self.image.get_rect()
-        self.rect.x = 600
-        self.rect.y = 680
         draw.add(self)
     def update(self):
         self.rect.x += self.move_x
+        xcoll()
         self.rect.y += self.move_y
+        ycoll()
         screen.blit(self.image, (self.rect.x, self.rect.y))
+
+def xcoll():
+    collision = pygame.sprite.spritecollide(player, plats, False)
+    for block in collision:
+        if player.move_x > 0:
+            player.rect.right = block.rect.left
+        if player.move_x < 0:
+            player.rect.left = block.rect.right
+def ycoll():
+    collision = pygame.sprite.spritecollide(player, plats, False)
+    player.onground = False
+    for block in collision:
+        if player.move_y == 0:
+            player.onground = True
+        if player.move_y < 0:
+            player.rect.top = block.rect.bottom
+            player.move_y = 0     
+            player.onground = False
+        if player.move_y > 0:
+            player.rect.bottom = block.rect.top
+            player.onground = True
+
+def build():
+    myx = 0
+    myy = 0
+    level = [
+            '#############################################',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#                                           #',
+            '#############################################']
+
+    for r in level:
+        for c in r:
+            if c == ' ':
+                pass
+            elif c == '#':
+                p = Platform(myx,myy)
+            myx += 20
+        myy += 20
+        myx = 0
 
 
 pygame.init()
@@ -49,6 +133,7 @@ pygame.mixer.init()
 
 player = Player()
 all_sprites_list.add(player)
+build()
 game_over = pygame.mixer.Sound("img/Game Over.wav")
 font = pygame.font.SysFont("brittanic", 25)
 scritta = font.render("Schiva più blocchi che puoi !!!", True, BLACK)
@@ -56,6 +141,8 @@ scritta = font.render("Schiva più blocchi che puoi !!!", True, BLACK)
 clock = pygame.time.Clock()
 
 player.rect.x = 150
+player.rect.y = (screen_height / 2) - 30
+
 
 while True:
     s += 1
@@ -108,7 +195,8 @@ while True:
             
     screen.fill(WHITE)
     all_sprites_list.draw(screen)
-    screen.blit(scritta,(100, 20))
+    plats.update()
+    screen.blit(scritta,(100, 30))
     pygame.display.flip()
     pygame.display.set_caption("Le avventure di Ben: livello 6" + "  Score: " + str(score))
     clock.tick(60)
